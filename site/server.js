b9e7b60e182fd8aac4c94b00d985d85d04aa433c
nie xin <niexin.study@gmail.com>
2014-07-12 14:12:29 +0200

    Start adding api

--- site/server.js
+++ site/server.js
@@ -22,3 +22,50 @@ var port = 4711;
 app.listen( port, function() {
 	console.log('Express server listening on port %d in %s mode', port, app.settings.env );
 });
+
+
+// define APIs
+app.get('/api', function( request, response) {
+	response.send('Library API is running');
+});
+
+app.get('/api/books', function( request, response ) {
+	return BookModel.find( function( err, books ) {
+		if ( !err ) {
+			return response.send( books );
+		} else {
+			return console.log( err );
+		}
+	});
+});
+
+app.post( '/api/books', function( request, response) {
+	var book = new BookModel({
+		title: request.body.title,
+		author: request.body.author,
+		releaseDate: request.body.releaseDate
+	});
+
+	book.save( function( err ) {
+		if ( !err ) {
+			return console.log( 'created' );
+		} else {
+			return console.log( err );
+		}
+	});
+
+	return response.send( book );
+});
+
+// connect to db
+mongoose.connect('mongodb://localhost/library_database');
+
+// db schema
+var Book = new mongoose.Schema({
+	title: String,
+	author: String,
+	releaseDate: Date 
+});
+
+// db model
+var BookModel = mongoose.model('Book', Book);
