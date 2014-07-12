7b7385b5ba50ea31a5e1b6505e43b1f967ac76e3
nie xin <niexin.study@gmail.com>
2014-07-13 01:15:15 +0200

    API completed

--- site/server.js
+++ site/server.js
@@ -39,11 +39,13 @@ app.get('/api/books', function( request, response ) {
 	});
 });
 
+// Insert a new book
 app.post( '/api/books', function( request, response) {
 	var book = new BookModel({
 		title: request.body.title,
 		author: request.body.author,
-		releaseDate: request.body.releaseDate
+		releaseDate: request.body.releaseDate,
+		keywords: request.body.keywords
 	});
 
 	book.save( function( err ) {
@@ -57,14 +59,65 @@ app.post( '/api/books', function( request, response) {
 	return response.send( book );
 });
 
+// get a book by id
+app.get( '/api/books/:id', function( request, response) {
+	return BookModel.findById( request.params.id, function( err, book ) {
+		if ( !err ) {
+			return response.send( book );
+		} else {
+			return console.log( err );
+		}
+	});
+});
+
+// update a book
+app.put( '/api/books/:id', function( request, response ) {
+	console.log( 'Updating book ' + request.body.title);
+	return BookModel.findById( request.params.id, function( err, book ) {
+		book.title = request.body.title;
+		book.author = request.body.author;
+		book.releaseDate = request.body.releaseDate;
+		book.keywords = request.body.keywords;
+
+		return book.save( function( err ) {
+			if ( !err ) {
+				console.log( 'book update' );
+			} else {
+				console.log( err );
+			}
+			return response.send( book );
+		});
+	});
+});
+
+// delete a book
+app.delete( '/api/books/:id', function( request, response ) {
+	console.log( 'Deleting book with id: ' + request.params.id );
+	return BookModel.findById( request.params.id, function( err, book ) {
+		return book.remove( function( err ) {
+			if ( !err ) {
+				console.log( 'Book removed' );
+				return response.send('');
+			} else {
+				console.log( err );
+			}
+		});
+	});
+});
+
 // connect to db
 mongoose.connect('mongodb://localhost/library_database');
 
 // db schema
+var Keywords = new mongoose.Schema({
+	keyword: String
+});
+
 var Book = new mongoose.Schema({
 	title: String,
 	author: String,
-	releaseDate: Date 
+	releaseDate: Date,
+	keywords: [ Keywords ]
 });
 
 // db model
